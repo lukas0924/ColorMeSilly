89dbe1fcbc1a249a4de85fd664bf02b851fdb362
lukas0924 <lukasgsmith23@gmail.com>
2022-05-12 14:14:57 -0400

    test

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ var c1=(0,0,0);
 var c2=(0,0,0);
 var cflag=0|1;
 
-const button1={button1: 'blue'};
-const button2={button2: '255,0,0'};
-const button3={button3: '255,255,0'};
+const button1={'btn blue': 'blue'};
+const button2={'btn yellow': '255,255,0'};
+const button3={'btn red': '255,0,0'};
 
 document.getElementById("blue").onclick=getValue();
-document.getElementById("red").onclick=getValue();
 document.getElementById("yellow").onclick=getValue();
+document.getElementById("red").onclick=getValue();
 
 function getValue(){
-	console.log(Object.values(button1));
-	console.log(Object.values(button2));
-	console.log(Object.values(button3));
+	console.log(Object.values('btn blue'));
+	console.log(Object.values('btn yellow'));
+	console.log(Object.values('btn red'));
 }
 
 function createColor(){
 var first=c1/2;
 var second=c2/2;
-var result=first + second;
+var result=first+second;
 return result;
 }
