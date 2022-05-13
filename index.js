34f2703a78e6847cef37a1131c27df4c0cc3e87b
lukas0924 <lukasgsmith23@gmail.com>
2022-05-13 13:25:34 -0400

    help

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var c1=(0,0,0);
 var c2=(0,0,0);
 var cflag=0|1;
 
-const button1={'btn blue': (0,0,255)};
-const button2={'btn yellow': (255,255,0)};
-const button3={'btn red': (255,0,0)};
+const button1={'btn blue': rgb(0,0,255)};
+const button2={'btn yellow': rgb(255,255,0)};
+const button3={'btn red': rgb(255,0,0)};
 
 function getValueBlue(){
 	console.log(Object.values(button1));
