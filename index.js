19da8b1e918908e79a3dfb2c4d7f8960141cb6c6
lukas0924 <lukasgsmith23@gmail.com>
2022-05-13 13:10:16 -0400

    i'm getting smarter

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var c1=(0,0,0);
 var c2=(0,0,0);
 var cflag=0|1;
 
-const button1={'btn blue': '0,0,255'};
-const button2={'btn yellow': '255,255,0'};
-const button3={'btn red': '255,0,0'};
+const button1={'btn blue': rgb(0,0,255)};
+const button2={'btn yellow': rgb(255,255,0)};
+const button3={'btn red': rgb(255,0,0)};
 
 function getValueBlue(){
 	console.log(Object.values(button1));
@@ -46,8 +46,8 @@ function getValueRed(){
 }
 
 function createColor(){
-var first=c1/2;
-var second=c2/2;
+var first=c1%2;
+var second=c2%2;
 var result=first+second;
 console.log("i am here");
 return result;
