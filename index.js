f68d60f7c994b852afd28af028482ba3f6fe05ae
lukas0924 <lukasgsmith23@gmail.com>
2022-05-15 21:08:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,43 @@ const button3={r:255, g:0, b:0};
 
 function getValueBlue(){
 	console.log(Object.values(button1));
-	if(cflag == 0){
+	if(cflag != 1){
 		c1=button1;
 		cflag=1;
 	}
 	else {
 		c2=button1;
 	}
+	console.log(Object.values(c2));
 	console.log(Object.values(c1));
 	createColor();
 }
 	
 function getValueYellow(){
 	console.log(Object.values(button2));
-	if(cflag == 0){
+	if(cflag != 1){
 		c1=button2;
 		cflag=1;
 	}
 	else {
 		c2=button2;
 	}
-	console.log("about to call createColor");
+	console.log(Object.values(c2));
+	console.log(Object.values(c1));
 	createColor();
 }
 
 function getValueRed(){
 	console.log(Object.values(button3));
-	if(cflag == 0){
+	if(cflag != 1){
 		c1=button3;
 		cflag=1;
 	}
 	else {
 		c2=button3;
 	}
-	console.log("about to call createColor");
+	console.log(Object.values(c2));
+	console.log(Object.values(c1));
 	createColor();
 }
 
@@ -49,6 +52,8 @@ function createColor(){
 var first=c1%2;
 var second=c2%2;
 var result=first+second;
+console.log(Object.values(c2));
+console.log(Object.values(c1));
 console.log("i am here");
 return result;
 }
