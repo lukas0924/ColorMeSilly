3ce69d9c42c7399bff327d09b1b7f0009f736c3c
lukas0924 <lukasgsmith23@gmail.com>
2022-05-23 13:38:01 -0400

    holy shit it works

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function checkColor(color){
 function resetColors(){
 	color1 = "";
 	color2 = "";
+	console.log("Colors have reset: "+color1+color2);
 	return 0;
 }
 
@@ -26,42 +27,55 @@ function combineColor(){
 	color1 = colorStringToRGB(color1);
 	color2 = colorStringToRGB(color2);
 	console.log("Got values: "+color1+" and "+color2);
-	createColor();
+	createColor(color1,color2);
 	resetColors();
-	console.log("Colors have reset: "+color1+color2);
 	return 0;
 }
 
 function colorStringToRGB(color){
 	if (color == "blue"){
-		blue = "0,0,255";
+		blue = [0,0,255];
 		return blue;
 	}
 	else if (color == "green"){
-		green = "0,255,0";
+		green = [0,255,0];
 		return green;
 	}
 	else if (color == "red"){
-		red = "255,0,0";
+		red = [255,0,0];
 		return red;
 	}
 	else if(color == "yellow"){
-		yellow = "255,255,0"
+		yellow = [255,255,0];
 		return yellow;
 	}
 	else if(color == "orange"){
-		orange = "255,165,0"
+		orange = [255,165,0];
 		return orange;
 	}
-	else if(color == "purple"){
-		purple = "128,0,128"
-		return purple;
+	else if(color == "magenta"){
+		magenta = [255,0,255];
+		return magenta;
+	}
+	else if(color == "cyan"){
+		cyan = [0,255,255];
+		return cyan;
 	}
 	console.log(color1,color2);
 	
 }
 
-function createColor(color1,color2,result){
-	result = color1 + color2;
-	console.log(result);
+function createColor(color1,color2){
+	let r1=color1[0];
+	let g1=color1[1];
+	let b1=color1[2];
+	let r2=color2[0];
+	let g2=color2[1];
+	let b2=color2[2];
+	let r3=Math.floor((r1+r2)/2);
+	let g3=Math.floor((g1+g2)/2);
+	let b3=Math.floor((b1+b2)/2);
+	
+	console.log(color1+" and "+color2);
+	console.log([r3,g3,b3]);
 }
